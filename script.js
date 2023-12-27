f0bf3b4dac24afd644c1819e47d699acf1ddb8bd
francispoliran <202180379@psu.palawan.edu.ph>
2023-12-27 17:52:24 +0800

    adding the search option

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const base_url = 'https://api.themoviedb.org/3';
 const api_url = base_url + '/discover/movie?sort_by=popularity.desc&'+api_key;
 const img_url = 'https://image.tmdb.org/t/p/w500';
 
+
+
+const searchURL = base_url + '/search/movie?'+ api_key;
+
 const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+
 
 getMovies(api_url);
 
@@ -54,3 +61,15 @@ function getColor(vote) {
         return 'red'
     }
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(api_url);
+    }
+
+})
