a194a29d84af991f8933d739c680299117064606
francispoliran <202180379@psu.palawan.edu.ph>
2023-12-27 16:59:29 +0800

    showing the movies

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ const base_url = 'https://api.themoviedb.org/3';
 const api_url = base_url + '/discover/movie?sort_by=popularity.desc&'+api_key;
 const img_url = 'https://image.tmdb.org/t/p/w500';
 
-
-
+const main = document.getElementById('main');
 
 getMovies(api_url);
 
 function getMovies(url){
 
     fetch(url).then(res => res.json()).then(data =>{
-        console.log(data);   
-
-        showMovies(data)
+        console.log(data.results)   
+        showMovies(data.results);
     })
 
 
@@ -21,6 +19,9 @@ function getMovies(url){
 
 
 function showMovies(data){
+    //add new space for movie
+    main.innerHTML = '';
+
     data.forEach(movie => {
         const {title, poster_path, vote_average, overview} = movie;
         const movieEl = document.createElement('div');
@@ -32,12 +33,24 @@ function showMovies(data){
             <span class="${getColor(vote_average)}">${vote_average}</span>
 
             <div class="overview">
+            <h3>Overview</h3>
            ${overview}
 
-        </div>`
-        
-    });
+        </div>
+        `
+
+        main.appendChild(movieEl); //add new elements
+    })
 
 
 }
 
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
