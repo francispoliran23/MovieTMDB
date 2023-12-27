ac1c6ee6067044ea2371bc94ea70c2a6468fe452
francispoliran <202180379@psu.palawan.edu.ph>
2023-12-27 16:02:25 +0800

    fuction get movies & url, testing

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const api_key = 'api_key=1bfdbff05c2698dc917dd28c08d41096';
 const base_url = 'https://api.themoviedb.org/3';
 const api_url = base_url + '/discover/movie?sort_by=popularity.desc&'+api_key;
 
+
+getMovies(api_url);
+
+function getMovies(url){
+
+    fetch(url).then(res => res.json()).then(data =>{
+        console.log(data);    })
+}
